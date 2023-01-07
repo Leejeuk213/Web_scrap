6429b627f39ada82c1030bd574f63a93bfa07580
Leejeuk213 <dlwpdnr213@naver.com>
2023-01-07 21:49:39 +0900

    second commit

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 from requests import get
 from bs4 import BeautifulSoup
 from extractors.wwr import extract_wwr_jobs
-from extractors.indeed import get_page_count
 from extractors.indeed import extract_indeed_jobs
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-from extractors.saramin import get_page_count
 from extractors.saramin import extract_saramin_jobs
-
+from extractors.file import save_to_file
 keyword = input("원하는 직업의 언어를 입력해주세요!")
 
 indeed = extract_indeed_jobs(keyword)
@@ -18,13 +16,6 @@ saramin = extract_saramin_jobs(keyword)
 
 jobs= indeed+saramin+wwr
 
-file = open(f"{keyword}.csv",'w',encoding='utf-8-sig')
-
-file.write("Position,Company,Location,URL\n",)
-
-for job in jobs :
-    file.write(f"{job['position']},{job['company']},{job['location']},{job['link']}\n")
-file.close()
-
+save_to_file(keyword,jobs)
     
  
